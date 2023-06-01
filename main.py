3522738d75be89b694e2ef48cb5fe84a9a04b28c
Ruslans Provorovs <potapskii@gmail.com>
2023-06-01 23:54:38 +0100

    "second commit"

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QApplication
 #from PyQt5 import uic
 import sys
 import viewer as vwr
-#import gvar
+# import gvar
 # https://www.youtube.com/watch?v=6zkOrq9YVik&list=PLCC34OHNcOtpmCA8s_dpPMvQLyHbvxocY&index=30
 # Thuesday 30
 
