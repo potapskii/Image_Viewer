0f2c32f10a219610179f7b713d375b01d9502d9e
Ruslans Provorovs <potapskii@gmail.com>
2023-06-04 12:03:03 +0100

    Image Viewer part2

--- gvar.py
+++ gvar.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-str = "Rusel Susel Test"
+width, height = 591, 581
--- main.py
+++ main.py
@@ -3,16 +3,14 @@ from PyQt5.QtWidgets import QApplication
 #from PyQt5 import uic
 import sys
 import viewer as vwr
-# import gvar
-# https://www.youtube.com/watch?v=6zkOrq9YVik&list=PLCC34OHNcOtpmCA8s_dpPMvQLyHbvxocY&index=30
-# Thuesday 30
+import gvar
 
 
 def main():
     app = QApplication(sys.argv)
     UIWindow = vwr.UI()
     UIWindow.setWindowTitle('Susel Image Viewer')
-    UIWindow.setFixedSize(591, 581)
+    UIWindow.setFixedSize(gvar.width, gvar.height)
     app.exec_()
 
 if __name__ == "__main__":
